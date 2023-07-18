9bcf492558937a37832e453fae9dd3e0dc636cbf
Jhon Eric <jhonerix23@gmail.com>
2023-07-18 19:03:08 +0800

    modify titles

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SectionTitle = ({title}) => {
   return (
-    <div className="title flex mb-32">
+    <div className="title flex mb-32 w-full justify-center">
         <h1 className='text-[2rem] text-fonts font-extrabold py-0.5 border-b-4 border-secondary'>{title}</h1>
     </div>
   )
